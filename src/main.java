5cb3cf058e803460f321c4598f88c5046255b9da
Ignatov Artem <Ignatoff1985@ukr.net>
2022-07-31 14:01:36 +0300

    added .gitignore file

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+        // тут будет выполняться программа
+        System.out.println("Hello World!");
+        System.out.println("My name is Artem");
+        System.out.println(5 + 10);
+        System.out.println();
+        int x = 1;
+        int y = x + 1;
+        System.out.println(y);
+    }
+}
